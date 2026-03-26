ab84217260cd0e8f402da0b18a4d01d19c984fdf
dominich <giang@murror.app>
2026-03-26 17:30:14 +0700

    feat: replace FloatingCompanion and ByteGuide with PetGuide system

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,29 @@
 import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
+import MeetYourPet from '@/components/MeetYourPet'
 import HowItWorks from '@/components/HowItWorks'
 import Features from '@/components/Features'
 import MidCTA from '@/components/MidCTA'
 import SkillTreePreview from '@/components/SkillTreePreview'
 import Testimonials from '@/components/Testimonials'
-import MeetYourPet from '@/components/MeetYourPet'
 import FinalCTA from '@/components/FinalCTA'
-import ByteGuide from '@/components/ByteGuide'
+import CharacterPicker from '@/components/CharacterPicker'
+import PetGuide from '@/components/PetGuide'
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative">
+      <CharacterPicker />
+      <PetGuide />
       <Nav />
       <Hero />
+      <MeetYourPet />
       <HowItWorks />
       <Features />
       <MidCTA />
       <SkillTreePreview />
       <Testimonials />
-      <MeetYourPet />
       <FinalCTA />
-      <ByteGuide />
     </main>
   )
 }
