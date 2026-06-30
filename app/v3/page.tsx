c1ed0fea9a1a43baba59aa2067535224447ddc1c
Mona <nguyen@murror.app>
2026-06-30 16:47:19 +0700

    v3: move pet cast to the marquee's bottom line + tighter spacing

--- app/v3/page.tsx
+++ app/v3/page.tsx
@@ -30,8 +30,8 @@ export default function V3Page() {
       <Nav />
       <main className="v3-skewer">
         <Hero />
-        <PetBand />
         <Marquee />
+        <PetBand />
         <Loop />
         <Environment />
         <Departments />
